09a8e1a99d7b253cb2296ca41b6a2a954d6f0cb8
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-07-15 01:14:02 +0200

    feat(rocket): add trait to mount routes via func

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,21 @@ fn index() -> &'static str {
     "Hello, World!"
 }
 
+trait Registerable {
+    fn mount_fn(self, func: &dyn Fn(Self) -> Self) -> Self;
+}
+
+impl Registerable for rocket::Rocket<rocket::Build> {
+    fn mount_fn(self, func: &dyn Fn(Self) -> Self) -> Self {
+        func(self)
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     dotenv::dotenv().ok();
-    let mut instance = rocket::build().mount("/", routes![index]);
 
-    instance = api::mount(instance);
-
-    instance
+    rocket::build()
+        .mount("/", routes![index])
+        .mount_fn(&api::mount)
 }
