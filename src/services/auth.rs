ee6a9510511b3aafcad45967db38b285b0b61380
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2024-07-21 22:12:22 +0200

    feat(auth): create sessions for usage in JWTs

--- src/services/auth.rs
+++ src/services/auth.rs
@@ -1,17 +1,25 @@
 use anyhow::{Error, Result};
 use serde::Serialize;
 
-use crate::{jwt, repositories::User, utils::verify_password};
+use crate::{
+    jwt,
+    repositories::{AuthRepository, Session, User},
+    utils::verify_password,
+};
 
 use super::UserService;
 
 pub struct AuthService {
     user_service: UserService,
+    session_repository: AuthRepository,
 }
 
 impl AuthService {
     pub async fn new(user_service: UserService) -> AuthService {
-        AuthService { user_service }
+        AuthService {
+            user_service,
+            session_repository: AuthRepository::new().await,
+        }
     }
 
     pub async fn login(
@@ -19,7 +27,6 @@ impl AuthService {
         username: impl ToString,
         password: impl ToString,
     ) -> Result<TokenPair> {
-        println!("Uuuuuhm...");
         let Some(User {
             id: Some(id),
             password: user_password,
@@ -29,11 +36,29 @@ impl AuthService {
             return Err(Error::msg("User not found!"));
         };
 
-        if let Ok(true) = verify_password(&password.to_string(), &user_password) {
-            return TokenPair::new(id);
+        let Ok(true) = verify_password(&password.to_string(), &user_password) else {
+            return Err(Error::msg("credentials do not match"));
         };
 
-        Err(Error::msg("credentials do not match"))
+        let Ok(Some(Session { id: Some(id), .. })) =
+            self.session_repository.create_session(id).await
+        else {
+            return Err(Error::msg("failed to create session"));
+        };
+
+        TokenPair::new(id)
+    }
+
+    pub async fn fetch_user_for_session(&self, session_id: impl ToString) -> Option<User> {
+        let Ok(Some(Session { user_id, .. })) = self
+            .session_repository
+            .find_session_by_id(session_id.to_string())
+            .await
+        else {
+            return None;
+        };
+
+        self.user_service.find_user_by_id(user_id).await
     }
 }
 
